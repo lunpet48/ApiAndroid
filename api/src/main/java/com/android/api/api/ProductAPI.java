0cfb2c8841963c821f78d823a188fa689328f142
lunpet48 <phaboss231@gmail.com>
2023-05-11 23:47:29 +0700

    Update get image feature

--- api/src/main/java/com/android/api/api/ProductAPI.java
+++ api/src/main/java/com/android/api/api/ProductAPI.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -81,5 +85,14 @@ public class ProductAPI {
         storageService.store(image, path);
         return ResponseEntity.ok().body("Product are created!!!");
     }
+
+    @GetMapping("images/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<Resource> serverFile(@PathVariable String filename) {
+		Resource file = storageService.loadAsResource(filename);
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\"" + file.getFilename() + "\"")
+				.body(file);
+	}
 
 }
